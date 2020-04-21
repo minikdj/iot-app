9ce9b7ad6d82714742f339bf28571433854f8862
minikdj <minikdj11@gmail.com>
2020-04-21 16:51:59 -0400

    parsed db data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default class App extends React.Component {
 
+
 	state = {
 		loading: true,
 		allData: null
@@ -12,9 +13,7 @@ export default class App extends React.Component {
 		const response = await fetch(url);
 		const data = await response.json();
 		this.setState({allData: data, loading: false});
-		console.log(JSON.stringify(data))
-	
-		console.log(data[0])
+		let parsedData = parseData(data);
 	}
 
 	render() {
@@ -27,4 +26,25 @@ export default class App extends React.Component {
 	}
 
 }
+async function parseData(data) {
+	let cleanData = {
+		gardenID : [],
+		timestamp: [],
+		temperature: [],
+		humidity: [],
+		soilMoisture: [],
+		light: []
+	}
 
+	for (var i = 0; i < data.length; i++) {
+		cleanData.gardenID.push(data[i].GardenID);
+		cleanData.timestamp.push(data[i].timestamp);
+		cleanData.temperature.push(data[i].Payload.temperature);
+		cleanData.humidity.push(data[i].Payload.humidity);
+		cleanData.soilMoisture.push(data[i].Payload['soil moisture']);
+		cleanData.light.push(data[i].Payload.light);
+	}
+
+        console.log(cleanData)	
+	return data.Payload;
+}
